df6be9a7aa2fa57601b32adf92dae339cb286533
lamberthnt-stack <lamberthnt@vcu.edu>
2025-08-28 13:09:48 -0400

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,14 +1,30 @@
+
 //Select all keys 
 const keys = document.querySelectorAll('.key');
 
-// -- Listeners -- //
-//Add an event listner to each key
+// -- Watchers -- //
+//Add an event Watcher to all keys
 keys.forEach((key)=> {
     key.addEventListener('click', () => playNote(key));
 });
 
-// -- Handlers -- // 
+// -- Writers -- // 
 function playNote(key) {
+    // Get the note's letter value
     const noteAudio = document.getElementById(key.dataset.note);
+
+    // Reset the note's current time to 0
+    noteAudio.currentTime = 0;
+
+    // Playing notes 
     noteAudio.play();
+
+    // Styling purposes. 
+    key.classList.add('active');
+
+    //Listen for when the sound has ended.
+    noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active');
+    });
+
 }
